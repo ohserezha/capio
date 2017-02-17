49db23f5f6e645dce120005f209271794cfd24c7
Roman Fromrome <romanfromrome@gmail.com>
2017-02-17 16:02:43 +0800

    tap and long tap to focus added

---
+++ capio/Controllers/FocusZoomViewController.swift
@@ -0,0 +1,50 @@
+//
+//  FocusZoomViewController.swift
+//  capio
+//
+//  Created by Roman on 2/17/17.
+//  Copyright © 2017 theroman. All rights reserved.
+//
+
+import UIKit
+
+class FocusZoomViewController: UIViewController {
+    
+    @IBOutlet var blurView: UIVisualEffectView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        resetView()
+    }
+    
+    func scaleToAppear() {
+        self.view.alpha = 0
+        UIView.animate(withDuration: 0.6, delay: 0.2, options: .curveEaseInOut, animations: {
+            self.view.alpha = 1
+        })
+    }
+    
+    func scaleToDisolve() {
+        UIView.animate(withDuration: 0.6, delay: 0.2, options: .curveEaseInOut, animations: {
+            self.view.alpha = 0
+        })
+    }
+    
+    func resetView() {
+        self.view.removeFromSuperview()
+        self.view.alpha = 1
+    }
+}
